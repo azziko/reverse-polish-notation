3e5bb029bfe0fa2422153a2f4d2ffbcc8fb67a73
Aziz Sharipov <101587881+azziko@users.noreply.github.com>
2022-03-18 23:25:20 +0500

    fixing
    comma changed to dot + some comments added

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,21 +1,23 @@
+// operator and dot checkers:
 var operator = 1; 
 var dot = 0;
+
 function back() {
     var value = document.getElementById("display").value;
     document.getElementById("display").value = value.substr(0, value.length - 1);
 }
-function math(val) {
+function math(val) { //dynamically adds a character to display
     if (document.getElementById("display").value.length < 15) {
-        operator = 0
+        operator = 0 
         document.getElementById("display").value+=val;
     }
 }
-function oper(val) {
-    if (val == ',' && dot == 0) {
+function oper(val) { //dynamically adds an operator to display
+    if (val == '.' && dot == 0) {
         dot = 1
         document.getElementById("display").value+=val;
     } else {
-        if (document.getElementById("display").value.length < 15 && operator == 0 && val != ',') {
+        if (document.getElementById("display").value.length < 15 && operator == 0 && val != '.') {
             operator = 1
             dot = 0
             document.getElementById("display").value+=val;
